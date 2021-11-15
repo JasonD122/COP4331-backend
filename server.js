adb903bb5491bd3a1e3b3d7732cb1fc14dad0550
Paul Wood <pmwood@pm.me>
2021-11-15 15:41:07 -0500

    Port is retrieved from the env Defaults to 5000 if the PORT env var is not set

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');           
-const PORT = 5000;  
+const PORT = process.env.PORT || 5000;  
 
 
 const app = express();
-// app.set('port', 5000);
+app.set(PORT);
 app.use(cors());
 app.use(bodyParser.json());
 const MongoClient = require('mongodb').MongoClient;
