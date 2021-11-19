7945f3ff9a762bdd54e88934c65bc446dd2d385e
Paul Wood <pmwood@pm.me>
2021-11-19 15:53:56 -0500

    Updated Jason's impl for 2 enpoints
    - addCompetition
    - deleteCompetition

--- src/api/deleteCompetition.js
+++ src/api/deleteCompetition.js
@@ -1,40 +1,63 @@
-module.exports = function deleteCompetitionHandler(req, res, next) {
+module.exports = function deleteCompetition(server, req, res, next) {
   // incoming: SessionID or Competition _id
   // outgoing: error , joinCode
 	
+  const dbm = server.dbm;
   const { sid } = req.body;
 
   //should return user object
-  const user = sm.authorize(sid);
+  //const user = sm.authorize(sid);
   
   let error="";
-
-  
   try
   {
+    const userID = await dbm.sessions.findOne({ sid: sid });
     
-    // we use user object to find competition to delete
-    comp = await db.collection('Competition').find({ _id : user.inst});
-  
-    //for each team we find Users
-    comp.teams.forEach(element => {
-        
-     const result =  db.collection('Teams').find({_id: element} );
-  
-     const result1 =  db.collection('Users').deleteOne({ _id : result.user});
-  
-     const result2 =  db.collection('Teams').deleteOne({ _id : element});
+    const user = await dbm.users.findOne({ _id: userID.user });
+    
+    const compe = await dbm.competitions.findOne({_id : user.inst});
+    
+    const length1 = compe.teams.length;
+    
+    let counter = 0;
+
+    const arr = compe.teams;
+    
+    
+    
+    for(counter = 0; counter < length1; counter++){
       
+      var neeDelete = await dbm.teams.findOne({_id : arr[counter]});
+      console.log(neeDelete);
+      
+      var delete2 =  await dbm.users.deleteOne({_id:neeDelete.user})
+    }
+    
+
+    const delete1 = await dbm.teams.deleteMany({ _id: 
+
+        {
+
+          $in : compe.teams
+
+        }
+    
+    
+    
     });
 
-    const del = await db.collection('Competition').deleteOne(comp);
+    const delete3 = dbm.competitions.deleteOne({_id:compe._id});
 
-  }
-  catch(e)
-  {
-    res.status(500).json({message : e.toString()})
-  }
 
-  let ret = { error: error, joinCode:joinCode };
+  let ret = {userID:userID , user:user};
   res.status(200).json(ret);
+  
+}
+catch(err1){
+  
+  let ret= {error: err1.message}
+  res.status(450).json(ret);
+}
+
+
 }
