ed89cc7231746a89374eec9096b81b7661df25de
Paul Wood <pmwood@pm.me>
2021-11-23 12:48:22 -0500

    fix: exception when competition doesn't exist
    - Fixes an exception when an admin user tries to get the status history
      but no competition exists
    - Fixes any future exceptions by providing checks and default values for
      if a team doesn't have any instances/machines and if
      a instance/machine doesn't have any services

--- src/api/statusHistory.js
+++ src/api/statusHistory.js
@@ -4,15 +4,19 @@ module.exports = async function statusHistory (server, req, res, next) {
   const user = server.authedUser;
 
   let teamIds;
+  let response = {
+    error: "",
+    teams: [],
+  };
   
   switch(user.type) {
     case 'admin':
       let competition = await dbm.competitions.findOne({ _id: user.inst });
-      teamIds = competition.teams;
+      teamIds = (competition) ? competition.teams : [];
       break;
 
     case 'team':
-      teamIds = [ user.inst ];
+      teamIds = (user.inst) ? [ user.inst ] : [];
       break;
 
     default:
@@ -20,39 +24,42 @@ module.exports = async function statusHistory (server, req, res, next) {
       return;
   }
 
+  // teamIds is null or empty
   if (!teamIds) {
-    res.status(200).json({ error:"", machines: [] });
+    res.status(200).json(response);
     return;
   }
-
-  let response = {
-    error: "",
-    teams: [],
-  };
 
   let teams = await dbm.teams.find({ _id: { $in: teamIds } }).toArray();
   for (const team of teams) {
     // This is to be resilient against db changes or missing fields...
     let machines = [];
-    for (const machine of team.instances) {
-      let services = [];
 
-      for (const service of machine.services) {
-        services.push({
-          name: service.name || "",
-          port: service.port || 0,
-          status: service.status || false,
-          history: service.history || [],
-          upCount: service.upCount || 0,
-          downCount: service.downCount || 0,
-        });
+    // If the team doesn't have any instances, default to []
+    if (team.instances) {
+      for (const machine of team.instances) {
+        let services = [];
+
+        // If the machine doesn't have any services, default to []
+        if (machine.services) {
+          for (const service of machine.services) {
+            services.push({
+              name: service.name || "",
+              port: service.port || 0,
+              status: service.status || false,
+              history: service.history || [],
+              upCount: service.upCount || 0,
+              downCount: service.downCount || 0,
+            });
+          }
+        }
+
+        machines.push({
+          name: machine.name || "",
+          ip: machine.ip || 0,
+          services
+        })
       }
-
-      machines.push({
-        name: machine.name || "",
-        ip: machine.ip || 0,
-        services
-      })
     }
 
     response.teams.push({
