b71496ba980889f5ae905395036cdc1f3e6c056b
Paul Wood <pmwood@pm.me>
2021-11-28 22:58:44 -0500

    fix: addInstances throwing exception

--- src/api/addInstances.js
+++ src/api/addInstances.js
@@ -1,38 +1,59 @@
-module.exports = function addInstances (server, req, res, next) {
+module.exports = async function addInstances (server, req, res, next) {
 
   // incoming: sid, instances
   // outgoing:  error
 
-  let {instances, sid} = req.body;
-	let error = '';
-  const dbm= server.dbm
+  let { instances } = req.body;
+  const dbm= server.dbm;
 
   //User much be from a team
   const user = server.authedUser;
+  const reqMachines = (await dbm.competitions.findOne({})).machines;
 
-
-  for (const inst of instances) {
-    for (const service of inst.services) {
-      service.status = false;
-      service.history = [];
-      service.upCount = 0;
-      service.downCount = 0;
-    }
+  if (reqMachines.length != instances.length) {
+    res.status(400).json({ error: "Num required machines doesn't match num instances passed" });
+    return;
   }
 
-	try {
-    dbm.teams.updateOne(
-      { _id: user.inst }, 
-      { $set :{instances} }
-    );
+  let reqMachinesMap = {};
 
-	}
-	catch (e) {
-		error = e.toString();
-	}
+  for (const m of reqMachines) {
+    reqMachinesMap[m.name] = m; 
+  }
 
-	let ret = {error:error};
-	res.status(200).json(ret);
+  const createdInsts = [];
+  for (const inst of instances) {
+    const m = reqMachinesMap[inst.name]; 
+    // I'm lazy, get over the naming
+    const createdSrvs = [];
+    if (!m) {
+      res.status(400).json({ error: `Unknown machine: ${m.name}` });
+      return;
+    }
 
+    for (const srv of m.services) {
+      createdSrvs.push({
+        name: srv.name,
+        port: srv.port,
+        status: false,
+        history: [],
+        upCount: 0,
+        downCount: 0,
+      });
+    }
+
+    createdInsts.push({
+      name: inst.name,
+      ip: inst.ip,
+      services: createdSrvs,
+    });
+  }
+
+  await dbm.teams.updateOne(
+    { _id: user.inst },
+    { $push: { instances: { $each: createdInsts } } }
+  );
+
+  res.status(200).json({ error: "" });
 }
 
